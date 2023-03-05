f13986c23519ebded808bed2c82fe846e11e26d0
Alex Guerra <alexander.a.guerra@gmail.com>
2023-03-04 19:27:36 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lovely-ada
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lovely_ada/core_test.clj
@@ -0,0 +1,7 @@
+(ns lovely-ada.core-test
+  (:require [clojure.test :refer :all]
+            [lovely-ada.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
